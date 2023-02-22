f3f0e2f12e3707a3d8d207ef7c2b5e3534af93ba
fgorordo <flgorordo@gmail.com>
2023-02-22 13:46:47 -0300

    added private and public routes, routes guards and last path memory on login

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -3,26 +3,24 @@ import { useNavigate } from "react-router-dom";
 import { AuthContext } from "../context";
 
 export const LoginPage = () => {
-  const { login } = useContext(AuthContext);
+    const { login } = useContext(AuthContext);
 
-  const navigate = useNavigate();
-  const handleLogin = () => {
-    login("Fernando Gorordo");
-    navigate("/", {
-      replace: true
-    })
-  };
+    const navigate = useNavigate();
+    const handleLogin = () => {
+        const lastPath = localStorage.getItem("lastPath") || "/";
+        login("Fernando Gorordo");
+        navigate(lastPath, {
+            replace: true,
+        });
+    };
 
-  return (
-    <div className="container mt-5">
-      <h1>Login</h1>
-      <hr />
-      <button 
-        className="btn btn-primary"
-        onClick={handleLogin}
-      >
-        Login
-      </button>
-    </div>
-  )
-}
+    return (
+        <div className="container mt-5">
+            <h1>Login</h1>
+            <hr />
+            <button className="btn btn-primary" onClick={handleLogin}>
+                Login
+            </button>
+        </div>
+    );
+};
